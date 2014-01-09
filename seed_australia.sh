62f470c9880464c7ce8c4b7485959e5e9f2acd05
chris <chris@chris-OptiPlex-980.(none)>
2014-01-09 23:22:48 +1100

    first.

---
+++ seed_australia.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/tilestache-seed.py -c tilestache.cfg -l ex -b -44.7545 111.1926 -7.3298 155.2258 -e png 12 13 14 15
---
+++ start_tilestache.sh
@@ -0,0 +1,3 @@
+#Ubuntu 12
+# tilestache installed with easy_install
+python /usr/local/bin/tilestache-server.py -c tilestache.cfg
